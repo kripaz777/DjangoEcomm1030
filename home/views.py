d4bdc5ee34d84b546a2a5a9594214253081005a1
Aayush <aceraayush@gmail.com>
2023-12-21 11:24:40 +0545

    views

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .views import *
+from django.views.generic import View
+from .models import *
 # Create your views here.
+class BaseView(View):
+    views = {}
+
+class HomeView(BaseView):
+    def get(self,request):
+        self.views['categories'] = Category.objects.all()
+        return render(request,'index.html',self.views)
