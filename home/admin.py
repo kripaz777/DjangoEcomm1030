f423c65c244328098eab741a17f1eaa8b1847179
Aayush <aceraayush@gmail.com>
2024-01-17 10:55:09 +0545

    admin modification

--- home/admin.py
+++ home/admin.py
@@ -5,7 +5,16 @@ admin.site.register(Category)
 admin.site.register(Ad)
 admin.site.register(Slider)
 admin.site.register(Brand)
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name","category","brand","label","stock","price","discounted_price")
+    list_filter = ("category","brand","label","stock" )
+    search_fields = ("name","description")
+    # fields = ("price","discounted_price")
+    ordering = ("name", "category", "brand", "stock", "price")
+
+    # class Meta:
+
 admin.site.register(Contact)
 admin.site.register(ContactInfo)
 admin.site.register(ProductReview)
